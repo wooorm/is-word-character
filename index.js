d916480c03cca1d6a937b236337b6e5ec4794985
Titus Wormer <tituswormer@gmail.com>
2021-03-11 10:04:40 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Check if the given character code, or the character code at the first
-// character, is a word character.
+/**
+ * Check if the given character code, or the character code at the first
+ * character, is a word character.
+ *
+ * @param {string|number} character
+ * @returns {boolean} Whether `character` is a word character.
+ */
 export function isWordCharacter(character) {
   return /\w/.test(
     typeof character === 'number'
